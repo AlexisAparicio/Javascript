ffef7c7da41d795ac9f6794367e1dbfce8fb00e8
Bolivar Aparicio <alexiiz360@gmail.com>
2023-02-20 17:54:07 -0500

    updates

--- project1/html-css/app.js
+++ project1/html-css/app.js
@@ -2,14 +2,97 @@ const menuEmail=document.querySelector('.navbar-email');
 const desktopmenu=document.querySelector('.desktop-menu');
 const menuburger=document.querySelector('.menu');
 const menumobile=document.querySelector('.mobile-menu');
+const menucarrito=document.querySelector('.navbar-shopping-cart');
+const menucarritodesktp=document.querySelector('.product-detail');
+const CardsContainer=document.querySelector('.cards-container-main');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuburger.addEventListener('click',toggleMobileMenu);
+menucarrito.addEventListener('click',toggleCarritoAside);
 
-function toggleDesktopMenu(){
+function toggleDesktopMenu() {
+    const isAsideClosed = menucarritodesktp.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        menucarritodesktp.classList.add('inactive');
+    }
+    
     desktopmenu.classList.toggle('inactive');
+    }
+    function toggleMobileMenu() {
+    const isAsideClosed = menucarritodesktp.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        menucarritodesktp.classList.add('inactive'); 
+    }
+    
+    menumobile.classList.toggle('inactive');
+    }
+
+    function toggleCarritoAside() {
+    const isMobileMenuClosed = menumobile.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed) {
+        menumobile.classList.add('inactive'); 
+    }
+    
+    menucarritodesktp.classList.toggle('inactive');
+    }
+
+    const productList =[];
+    productList.push({
+        name: 'VirusT',
+        price:'450',
+        image: '../imgs/icon/virusT.jpeg',
+    });
+
+    productList.push({
+        name: 'VirusG',
+        price:'350',
+        image: '../imgs/icon/virusT.jpeg',
+    });
+
+    productList.push({
+        name: 'Virus Progenitor',
+        price:'1000',
+        image: '../imgs/icon/virusT.jpeg',
+    });
+
+    
+    function RenderProducts(arr){
+    //Recorrido de array y obtener el elemento
+    for(product of arr){
+    const productCard= document.createElement('div');
+    productCard.classList.add('product-card-main');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src',product.image);
+
+    const productInfo= document.createElement('div');
+    productInfo.classList.add('product-info-main');
+
+    const productInfoDiv= document.createElement('div');
+
+    const productPrice= document.createElement('p');
+        productPrice.innerHTML='$'+product.price;
+
+    const productName= document.createElement('p');
+    productName.innerHTML=product.name;
+
+    productInfoDiv.append(productPrice,productName);
+
+    const productinfoFigure= document.createElement('figure');
+    const productIMGCart = document.createElement('img');
+    productIMGCart.setAttribute('src','../imgs/curso-frontend-developer-practico/icons/bt_add_to_cart.svg');
+    
+    productinfoFigure.appendChild(productIMGCart);
+
+    productInfo.append(productInfoDiv,productinfoFigure);
+
+    productCard.append(productImg,productInfo);
+
+    CardsContainer.appendChild(productCard);
+}
 }
 
-function toggleMobileMenu(){
-    menumobile.classList.toggle('inactive');
-}
+RenderProducts(productList);
