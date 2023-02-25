b502b3c0bad0e9a073868ae7d200a3dd29e59c58
Bolivar Aparicio <alexiiz360@gmail.com>
2023-02-24 19:41:15 -0500

    updtupdate

--- app.js
+++ app.js
@@ -106,13 +106,6 @@ function toggleDesktopMenu() {
         price:'1000',
         image: 'https://github.com/AlexisAparicio/Javascript/blob/abd41643cc0b1cb3efe98b4f030f5a829323f96f/project1/imgs/icon/virusT.jpeg',
     });
-
-    productList.push({
-        name: 'Virus Progenitor',
-        price:'1000',
-        image: 'https://github.com/AlexisAparicio/Javascript/blob/abd41643cc0b1cb3efe98b4f030f5a829323f96f/project1/imgs/icon/virusT.jpeg',
-    });
-
     
     function RenderProducts(arr){
     //Recorrido de array y obtener el elemento
